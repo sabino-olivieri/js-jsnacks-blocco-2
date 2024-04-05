399da2c815b5c58df6df6295d2c123836ef4c594
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-04-05 11:44:25 +0200

    feat: print on body

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -2,8 +2,11 @@ const numUser = parseInt(prompt("Dimmi un numero:"));
 
 const lengthArray = 10;
 
+const bodyElem = document.querySelector("body");
+
 for (let i = 0; i < numUser; i++) {
     const newArray = genArray(lengthArray);
 
     console.log(newArray);
+    bodyElem.innerHTML += `<h6>${newArray}</h6>`;
 }
