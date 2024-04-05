6f59d0a2471fa9d71c89aa1593aa5f4216f8ef0e
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-04-05 12:41:31 +0200

    add: comments

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,19 +1,29 @@
+// chiedo parola1 all'utente
 const word1 = prompt("Dimmi una parola");
 
+// chiedo parola2 all'utente
 const word2 = prompt("Dimmi un'altra parola");
 
-let strMessage = "";
+// creo cariabile per il risultato
+let strMessage = "La parola più lunga è: ";
 
+// prendo elemento body
 const bodyElem = document.querySelector("body");
+
 
 if(wordSameLength(word1, word2)) {
 
-    strMessage = word1 + " " + word2;
+    strMessage = "Hanno la stessa lunghezza " + word1 + " " + word2;
+
 } else if (word1IsMoreLonger(word1, word2)) {
-    strMessage = word1;
+
+    strMessage +=  word1;
+
 } else {
-    strMessage = word2;
+
+    strMessage += word2;
+
 }
 
-
+// stampo dento al body
 bodyElem.innerHTML = `<h1>${strMessage}</h1>`
